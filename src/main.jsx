3fe8d780c15c8005bbe83d69c54e1476f628dd04
Jimmy Hayashihara <jimmyhayashi@live.com>
2024-08-16 08:40:21 +0200

    Added Loader while loading Iphone 3D

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://4a65e1f2cd0f520a08b60f4812b3ed21@o4507785589424128.ingest.de.sentry.io/4507785591521360",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
